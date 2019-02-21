d944de5115957d098f4a4f56236cedebf35d8a28
Sander Brugge <sander.brugge@moovly.com>
2019-02-21 10:18:23 +0100

    remove default header for fullscreen support

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -6,10 +6,6 @@ import MapboxGL from '@mapbox/react-native-mapbox-gl';
 MapboxGL.setAccessToken('sk.eyJ1Ijoic2FuZGVyYnJ1Z2dlIiwiYSI6ImNqc2Via2oyYjB3cHY0M3RiYmhtY2NqNmIifQ.vJXyD8y5dZNOIS6JAKZWJw');
 
 export default class Home extends Component<{}> {
-  navigationOptions = {
-    header: null
-  }
-
   render() {
     return (
       <View style={styles.container}>
