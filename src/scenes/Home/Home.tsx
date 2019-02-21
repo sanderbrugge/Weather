847fb8e9ccdf51c9bfab50f1f00080150c7b348c
Sander Brugge <sander.brugge@moovly.com>
2019-02-21 10:16:18 +0100

    link mapbox to ios + display basic map on screen

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -1,10 +1,31 @@
-import * as React from 'react';
-import { Text } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+// @ts-ignore no official @types declaration files
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
 
-const Home: React.FC = () => {
-  return (
-    <Text>Homescreen</Text>
-  );
+MapboxGL.setAccessToken('sk.eyJ1Ijoic2FuZGVyYnJ1Z2dlIiwiYSI6ImNqc2Via2oyYjB3cHY0M3RiYmhtY2NqNmIifQ.vJXyD8y5dZNOIS6JAKZWJw');
+
+export default class Home extends Component<{}> {
+  navigationOptions = {
+    header: null
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapboxGL.MapView
+            styleURL={MapboxGL.StyleURL.Street}
+            zoomLevel={15}
+            centerCoordinate={[11.256, 43.770]}
+            style={styles.container}>
+        </MapboxGL.MapView>
+      </View>
+    );
+  }
 }
 
-export default Home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
