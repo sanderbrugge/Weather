17e9d76c6caff4325904242b74273582c99d441d
Sander Brugge <sander.brugge@moovly.com>
2019-02-23 15:51:14 +0100

    add base style of colors

---
+++ src/styles/base.ts
@@ -0,0 +1,19 @@
+import { Dimensions } from 'react-native'
+
+export const dimensions = {
+  fullHeight: Dimensions.get('window').height,
+  fullWidth: Dimensions.get('window').width,
+  headerHeight: 210,
+  isLandscape: Dimensions.get('window').height < Dimensions.get('window').width,
+}
+
+export const colors = {
+  black: '#000000',
+  white: '#ffffff',
+  grey: '#a5a5a5',
+  red: '#DC143C',
+  blue: '#4169E1',
+  green: '#008000',
+  yellow: '#FFFF00',
+  transparent: 'transparent',
+}
